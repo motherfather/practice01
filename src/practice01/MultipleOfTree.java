6f039a1aded7f475fe09bafda3417e79f5ab0fae
kickscar <kickscar@gmail.com>
2016-03-08 20:57:01 +0900

    re-commit

---
+++ src/practice01/MultipleOfTree.java
@@ -0,0 +1,9 @@
+package practice01;
+
+
+public class MultipleOfTree {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/practice01/Prob2.java
@@ -0,0 +1,9 @@
+package practice01;
+
+
+public class Prob2 {
+
+	public static void main(String[] args) {
+	}
+
+}
