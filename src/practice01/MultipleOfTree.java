18936a5f44eb0156f649160b39eb86a32c02a61b
kickscar <kickscar@gmail.com>
2016-09-06 08:37:03 +0900

    수

--- src/practice01/MultipleOfTree.java
+++ src/practice01/MultipleOfTree.java
@@ -6,14 +6,6 @@ public class MultipleOfTree {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-		
-		System.out.print( "수를 입력하시오 : " );
-		int number = scanner.nextInt();
-		
-		if( number % 3 == 0 ) {
-			System.out.println( "3의 배수입니다." );
-		}
-		
 		scanner.close();
 	}
 }
