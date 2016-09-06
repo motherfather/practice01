358e2eb52e1125366eaf3e5e47cd0bb71c5c02f9
BITUSER <BITUSER@BITPC>
2016-09-06 17:27:05 +0900

    GuessNumber finish 160906 17:27

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -1,5 +1,20 @@
 package practice01;
 public class Game369 {
 	public static void main(String[] args) {
+		
+		int number = 36942;
+		String s = String.valueOf(number);
+		
+		System.out.println(s.length());
+		char c = s.charAt(2);
+		System.out.println(c);
+		
+		int clapcount = 0;
+		if(c == '3' || c == '6' || c == '9'){
+			clapcount++;
+		}
+		
+		
+		
 	}
 }
--- src/practice01/GuessNumber.java
+++ src/practice01/GuessNumber.java
@@ -1,11 +1,54 @@
 package practice01;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
 	public static void main(String[] args) {
-		
+		Random r = new Random();
+		int guessNumber = r.nextInt(100) + 1;
 		Scanner scanner = new Scanner( System.in );
+		
+		int min = 1;
+		int max = 100;
+		System.out.println("수를 결정하였습니다. 맞춰보세요." + '\n' + min + "-" + max);
+		int i = 1;
+		while(true){
+			System.out.print(i + ">>");
+			int num = scanner.nextInt();
+			
+			if(num == guessNumber){
+				System.out.println("맞았습니다." + '\n' + "다시하시겠습니까(y/n)");
+				String answer = scanner.next();
+				if(answer.equals("y")){
+					guessNumber = r.nextInt(100) + 1;
+					min = 1;
+					max = 100;
+					System.out.println("수를 결정하였습니다. 맞춰보세요." + '\n' + min + "-" + max);
+					i = 1;
+					continue;
+				}else{
+					break;
+				}
+			}else{
+				if(num > guessNumber){
+					System.out.println("더 낮게");
+					if(max > num){
+						max = num;
+					}
+					System.out.println(min + "-" + max);
+				}else{
+					System.out.println("더 높게");
+					if(min < num){
+						min = num;
+					}
+					System.out.println(min + "-" + max);
+				}
+				
+			}
+			i++;
+		}
+		
 		scanner.close();
 	}
 }
