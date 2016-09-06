94d97d3b3e6621ed0b4a06497179ec97c56a1f2e
BITUSER <BITUSER@BITPC>
2016-09-06 18:25:23 +0900

    1111

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -2,19 +2,33 @@ package practice01;
 public class Game369 {
 	public static void main(String[] args) {
 		
-		int number = 36942;
-		String s = String.valueOf(number);
-		
-		System.out.println(s.length());
-		char c = s.charAt(2);
-		System.out.println(c);
-		
-		int clapcount = 0;
-		if(c == '3' || c == '6' || c == '9'){
-			clapcount++;
+		for(int i = 1; i ){
+			int num = 1;
+			String s = String.valueOf(num);
+			for(int i = 0; i < 2; i++){
+				char c = s.charAt(i);
+				
+				int clap = 0;
+				if(c == '3' || c == '6' || c == '9'){
+					clap++;
+				}
+				
+				System.out.println("c");
+			}
 		}
 		
 		
 		
 	}
 }
+
+//			int number = 36942;
+//			String s = String.valueOf(number);
+//		
+//			System.out.println(s.length());
+//			char c = s.charAt(2);
+//			System.out.println(c);
+//		
+//			int clapcount = 0;
+//			if(c == '3' || c == '6' || c == '9'){
+//			clapcount++;
--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -1,9 +1,15 @@
 package practice01;
 
-
 public class Prob2 {
 
 	public static void main(String[] args) {
+		for(int ii = 1; ii <= 10; ii++){
+			for(int i = 0, j = ii; i < 10; i++){
+				System.out.print(j + " ");
+				j++;
+			}
+			System.out.println("");
+		}
 	}
 
 }
