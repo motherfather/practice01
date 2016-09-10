c209c96ace8c4e4ade577fa835852a5d3622e49b
motherfather <mother@father>
2016-09-10 16:48:15 +0900

    160910 16:48...

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -2,26 +2,27 @@ package practice01;
 public class Game369 {
 	public static void main(String[] args) {
 		
-		for(int i = 1; i ){
-			int num = 1;
-			String s = String.valueOf(num);
-			for(int i = 0; i < 2; i++){
-				char c = s.charAt(i);
-				
-				int clap = 0;
-				if(c == '3' || c == '6' || c == '9'){
-					clap++;
-				}
-				
-				System.out.println("c");
-			}
+		for(int i = 1; i <= 99; i++){
+			String s = String.valueOf(i);
+			int clap = 0;
+			
+//			for(int j = 0; j < s.length(); j++){
+//				char c = s.charAt(j);
+//				
+//				if(c == '3' || c == '6' || c == '9'){
+//					clap++;
+//				}
+//				if(clap > 1){
+//					System.out.print("s");
+//					for(int k = 1; k < clap; k++){
+//						System.out.print("짝");
+//				
+//			}
+//				}
+//			}
 		}
-		
-		
-		
 	}
 }
-
 //			int number = 36942;
 //			String s = String.valueOf(number);
 //		
