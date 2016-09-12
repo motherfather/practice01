cb6153729e865b6faa9922f6158a6eaacccb6e44
motherfather <mother@father>
2016-09-12 19:52:48 +0900

    MultipleOfTree update!

--- src/practice01/MultipleOfTree.java
+++ src/practice01/MultipleOfTree.java
@@ -7,11 +7,17 @@ public class MultipleOfTree {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		System.out.print("수를 입력하시오 : ");
-		int num = scanner.nextInt();
-		if(num % 3 == 0){
-			System.out.println("3의 배수입니다.");
-		}else{
-			System.out.println("3의 배수가 아닙니다.");
+		String num = scanner.nextLine();
+		char ch = num.charAt(0);
+		if(!Character.isDigit(ch)){
+			System.out.println("숫자가 아닙니다.");
+		}
+		else{
+			if(ch % 3 == 0){
+				System.out.println("3의 배수입니다.");
+			}else{
+				System.out.println("3의 배수가 아닙니다.");
+			}
 		}
 		scanner.close();
 	}
